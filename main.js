b8f9bcb91a93d6f2199e70c116582721596a147b
Jesue Junior <jesuesousa@gmail.com>
2013-05-17 06:43:20 -0300

    Ajustes nos pacotes e adiçào do projetos

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var app = express();
 
 app.configure(function(){
     app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.set('view options', {layout: false});
+    app.set('view engine', 'jade');
+    app.set('view options', {pretty: false, layout: false});
     app.use(express.static(__dirname + '/static'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
@@ -16,6 +16,10 @@ app.configure(function(){
 
 app.get('/', function(req, res){
     res.render('index');
+});
+
+app.get('/projetos', function(req, res){
+   res.render('projetos');
 });
 
 app.listen(3000);
