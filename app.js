71f54ce69fb572326c8102d6f62b794d4e121f9e
Jesue Junior <jesuesousa@gmail.com>
2013-05-17 07:55:41 -0300

    Padronizando de acordo com o express

---
+++ app.js
@@ -0,0 +1,37 @@
+
+var express = require('express');
+var app = express();
+
+    console.log("Servidor iniciado com sucesso!!!");
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {pretty: false, layout: false});
+    app.use(express.static(__dirname + '/static'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
+
+
+app.get('/', function(req, res){
+    res.render('index', {title: "Home"});
+});
+
+app.get('/projetos', function(req, res){
+   res.render('projetos', {title: "Projetos"});
+});
+
+app.get('/about', function(req, res){
+    res.render('about', {title: "Sobre"});
+});
+
+app.get('/contato', function(req, res){
+    res.render('contato', {title: "Contato"});
+});
+
+app.get('/blog', function(req, res){
+    res.render('blog', {title: "Blog"});
+});
+
+app.listen(3000);
