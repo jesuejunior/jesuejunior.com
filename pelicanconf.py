2e1cdf409bfb246a281edf4f1dbefe0fac296a67
Jesue Junior <jesuesousa@gmail.com>
2019-07-19 18:41:05 -0300

    Upgrading pelican version

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,17 +2,21 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'JJ'
-SITENAME = u'Jesué Junior'
+AUTHOR = 'JJ'
+SITENAME = 'Jesué Junior'
 SITEURL = ''
 
 PATH = 'content'
-
 THEME = 'themes/jj'
-
 TIMEZONE = 'America/Sao_Paulo'
 
-DEFAULT_LANG = u'pt-br'
+DEFAULT_LANG = 'en'
+
+ARTICLE_PATHS = ['articles']
+ARTICLE_URL = '{slug}'
+STATIC_PATHS = ['img', 'extra/favicon.ico', 'extra/CNAME', 'extra/robots.txt',
+                'extra/jesuejunior.pub']
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,11 +24,6 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-ARTICLE_PATHS = ['articles']
-ARTICLE_URL = '{slug}'
-STATIC_PATHS = ['img', 'extra/favicon.ico', 'extra/CNAME', 'extra/robots.txt']
-
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -36,29 +35,14 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-PLUGIN_PATHS = ["plugins"]
-PLUGINS = ["sitemap", "backreftranslate"]
-
-SITEMAP = {
-        'format': 'xml',
-        'priorities': {
-            'articles': 0.5,
-            'indexes': 0.5,
-            'pages': 0.5
-        },
-        'changefreqs': {
-            'articles': 'weekly',
-            'indexes': 'daily',
-            'pages': 'monthly'
-        }
-}
 
 TYPOGRIFY = True
-
 GOOGLE_ANALYTICS = True
-
 LOAD_CONTENT_CACHE = False
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
